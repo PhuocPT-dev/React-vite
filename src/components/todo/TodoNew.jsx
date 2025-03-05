c7be353461df2c485bfcf2648b2bfffca479249d
Phan Thanh Phuoc <PhuocPT-dev>
2025-03-05 22:22:16 +0700

    video 36

--- src/components/todo/TodoNew.jsx
+++ src/components/todo/TodoNew.jsx
@@ -1,14 +1,30 @@
 
 const TodoNew = (props) => {
-    console.log(">>> check point : " , props);
+    console.log(">>> check point : ", props);
     const { addNewTodo } = props
 
-    // addNewTodo("Phước")
+    // addNewTodo("Phước") 
+    // //fire
+    const handleClick = () => {
+        alert("click me")
+    }
+    //cách 1
+    // const handleOnChange = (event) => {
+    //     console.log(">>> handleOnChange", event.target.value);
+
+    const handleOnChange = (name) => {
+        console.log(">>> handleOnChange", name);
+
+    }
     return (
         <div className='todo-new'>
-            <input type="text" />
-            <button>Add</button>
-      </div>
+            <input type="text"
+                onChange={(event) => {handleOnChange(event.target.value)}}
+            />
+            <button style={{ cursor: "pointer" }}
+                onClick={handleClick}
+            >Add</button>
+        </div>
     )
 }
 
