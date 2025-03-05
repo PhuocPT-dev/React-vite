aa291560ac79f682ec3f126a8ae2f45c497e0ae0
Phan Thanh Phuoc <PhuocPT-dev>
2025-03-05 21:50:49 +0700

    video 35

--- src/components/todo/TodoNew.jsx
+++ src/components/todo/TodoNew.jsx
@@ -1,5 +1,9 @@
 
-const TodoNew = () => {
+const TodoNew = (props) => {
+    console.log(">>> check point : " , props);
+    const { addNewTodo } = props
+
+    // addNewTodo("Phước")
     return (
         <div className='todo-new'>
             <input type="text" />
