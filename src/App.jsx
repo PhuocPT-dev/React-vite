154fad0337b64e3a4aa39695fa10fd0bee64150e
Phan Thanh Phuoc <PhuocPT-dev>
2025-03-06 14:27:28 +0700

    video 40

--- src/App.jsx
+++ src/App.jsx
@@ -7,8 +7,8 @@ import { useState } from 'react';
 const App = () => {
 
   const [todoList, setTodoList] = useState([
-    { id: 1, name: "Learning React" },
-    { id: 2, name: "Watching Youtube" },
+    // { id: 1, name: "Learning React" },
+    // { id: 2, name: "Watching Youtube" },
   ])
 
   const hoidanit = "Eric MU";
