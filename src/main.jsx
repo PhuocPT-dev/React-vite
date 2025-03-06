29617c8f969fcb0fb2005956a80a740c3c139a44
Phan Thanh Phuoc <PhuocPT-dev>
2025-03-06 21:16:47 +0700

    video 46

--- src/main.jsx
+++ src/main.jsx
@@ -5,6 +5,12 @@ import {
   createBrowserRouter,
   RouterProvider,
 } from "react-router-dom";
+import LoginPage from './pages/login';
+import RegisterPage from './pages/register';
+import UsersPage from './pages/user';
+import ProductsPage from './pages/products';
+
+import './styles/global.css'
 
 const router = createBrowserRouter([
   {
@@ -13,19 +19,19 @@ const router = createBrowserRouter([
   },
   {
     path : "/login",
-    element: <div>login page</div>
+    element: <LoginPage/>
   },
   {
     path : "/register",
-    element: <div>register page</div>
+    element: <RegisterPage/>
   },
   {
     path : "/users",
-    element: <div>users page</div>
+    element: <UsersPage/>
   },
   {
     path : "/products",
-    element: <div>products page</div>
+    element: <ProductsPage/>
   },
 ]);
 
