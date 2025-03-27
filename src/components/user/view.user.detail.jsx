607514f41455f532f57462c3225cae2ed3868638
Phan Thanh Phuoc <PhuocPT-dev>
2025-03-27 17:26:11 +0700

    video 82

--- src/components/user/view.user.detail.jsx
+++ src/components/user/view.user.detail.jsx
@@ -9,7 +9,9 @@ const ViewUserDetail = (props) => {
     } = props
     return (
         <>
+
             <Drawer title="Chi tiết User"
+                width={"40vw"}
                 onClose={() => {
                     setDataDetail(null);
                     setIsDetailOpen(false);
@@ -21,13 +23,36 @@ const ViewUserDetail = (props) => {
                     <p>Full name: {dataDetail.fullName}</p>
                     <br />
                     <p>Phone number: {dataDetail.phone}</p>
+                    <br />
+                    <p>Avatar:</p>
+                    <div>
+                        <img height={100} width={150}
+                            src={`${import.meta.env.VITE_BACKEND_URL}/images/avatar/${dataDetail.avatar}`}
+                        />
+                    </div>
+                    <div>
+                        <label htmlFor='btnUpload'
+                            style={{
+                                display: "block",
+                                width: "fit-content",
+                                marginTop: "15px",
+                                padding: "5px 10px",
+                                background: "orange",
+                                borderRadius: "5px",
+                                cursor: "pointer"
+                            }}>
+                            Upload Avatar
+                        </label>
+                        
+                        <input type='file' hidden id='btnUpload' />
+                    </div>
                 </> :
                     <>
                         <p>
                             Không có dữ liệu
                         </p>
                     </>
-                    }
+                }
             </Drawer>
         </>
     );
